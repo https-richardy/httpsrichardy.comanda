a46d6513a8caa5f505747674573e02bc02155683
Richard Garcia <code.richardy@gmail.com>
2026-02-21 20:01:13 -0300

    refactor: streamline method signatures in ProfilesClient for consistency

--- Artifacts/Comanda.Internal.Sdk/source/clients/profiles-client.ts
+++ Artifacts/Comanda.Internal.Sdk/source/clients/profiles-client.ts
@@ -12,8 +12,7 @@ export class ProfilesClient {
         this.httpClient = httpClient;
     }
 
-    public async createOwner(parameters: ProfileCreationScheme): Promise<Result<ProfileScheme>>
-    {
+    public async createOwner(parameters: ProfileCreationScheme): Promise<Result<ProfileScheme>> {
         const response = await this.httpClient.post("/api/v1/profiles/owner", parameters);
         if (response.status < 200 || response.status >= 300) {
             return Result.failure(Error.from(response.data.code, response.data.description));
@@ -22,8 +21,7 @@ export class ProfilesClient {
         return Result.success(response.data);
     }
 
-    public async createCustomer(parameters: ProfileCreationScheme): Promise<Result<void>>
-    {
+    public async createCustomer(parameters: ProfileCreationScheme): Promise<Result<void>> {
         const response = await this.httpClient.post("/api/v1/profiles/owner", parameters);
         if (response.status < 200 || response.status >= 300) {
             return Result.failure(Error.from(response.data.code, response.data.description));
