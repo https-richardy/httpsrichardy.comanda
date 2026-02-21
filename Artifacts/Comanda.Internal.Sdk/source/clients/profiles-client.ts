948a9ea2e47e5ca7ec4ae2dfb6e836334fee09fb
Richard Garcia <code.richardy@gmail.com>
2026-02-21 19:23:27 -0300

    feature: this commit introduces a new publishable artifact to comanda, a typescript SDK for consumption by the comanda ecosystem.

---
+++ Artifacts/Comanda.Internal.Sdk/source/clients/profiles-client.ts
@@ -0,0 +1,9 @@
+import { ProfileCreationScheme } from "../transport/profiles/profile-creation-scheme";
+
+export class ProfilesClient {
+    public static async createOwner(parameters: ProfileCreationScheme): Promise<void> {
+    }
+
+    public static async createCustomer(parameters: ProfileCreationScheme): Promise<void> {
+    }
+}
