2e81ef11611a52034d1d17101113e05e057540fb
Richard Garcia <code.richardy@gmail.com>
2026-02-21 11:06:20 -0300

    feature(#2): includes the "address" property in the schemes records, allowing the address associated with the order to be stored during its creation and representation.

--- Boundaries/Comanda.Orders/Source/Comanda.Orders.Application/Payloads/Order/OrderScheme.cs
+++ Boundaries/Comanda.Orders/Source/Comanda.Orders.Application/Payloads/Order/OrderScheme.cs
@@ -5,6 +5,7 @@ public sealed record OrderScheme
     public string Identifier { get; init; } = default!;
     public string Code { get; init; } = default!;
 
+    public Address? Address { get; init; } = default!;
     public Status Status { get; init; }
     public Priority Priority { get; init; }
     public Fulfillment Fulfillment { get; init; }
