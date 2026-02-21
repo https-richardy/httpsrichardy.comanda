990b505f2617064c5f17b8505be7aad421f71772
Richard Garcia <code.richardy@gmail.com>
2026-02-21 19:41:47 -0300

    feature: this commit introduces result pattern

--- Artifacts/Comanda.Internal.Sdk/source/clients/profiles-client.ts
+++ Artifacts/Comanda.Internal.Sdk/source/clients/profiles-client.ts
@@ -1,9 +1,34 @@
+import { AxiosInstance } from "axios";
+import { Result } from "../patterns/result";
+import { Error } from "../patterns/error";
+
 import { ProfileCreationScheme } from "../transport/profiles/profile-creation-scheme";
+import { ProfileScheme } from "../transport/profiles/profile-scheme";
 
 export class ProfilesClient {
-    public static async createOwner(parameters: ProfileCreationScheme): Promise<void> {
+    private readonly httpClient: AxiosInstance;
+
+    public constructor(httpClient: AxiosInstance) {
+        this.httpClient = httpClient;
     }
 
-    public static async createCustomer(parameters: ProfileCreationScheme): Promise<void> {
+    public async createOwner(parameters: ProfileCreationScheme): Promise<Result<ProfileScheme>>
+    {
+        const response = await this.httpClient.post("/api/v1/profiles/owner", parameters);
+        if (response.status < 200 || response.status >= 300) {
+            return Result.failure(Error.from(response.data.code, response.data.description));
+        }
+
+        return Result.success(response.data);
+    }
+
+    public async createCustomer(parameters: ProfileCreationScheme): Promise<Result<void>>
+    {
+        const response = await this.httpClient.post("/api/v1/profiles/owner", parameters);
+        if (response.status < 200 || response.status >= 300) {
+            return Result.failure(Error.from(response.data.code, response.data.description));
+        }
+
+        return Result.success(response.data);
     }
 }
