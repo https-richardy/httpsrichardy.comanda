5ad7887833f0ea23fd0b7466e7b0dfd352788a53
Richard Garcia <code.richardy@gmail.com>
2026-02-21 18:51:48 -0300

    feature: this commit introduces local storage keys constants

---
+++ UIs/Landing/constants/localstorage-constants.ts
@@ -0,0 +1,5 @@
+export default class LocalStorage {
+    public static readonly AccessToken = "comanda:access_token";
+    public static readonly RefreshToken = "comanda:refresh_token";
+    public static readonly Principal = "comanda:principal";
+}
