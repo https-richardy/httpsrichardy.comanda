00a9b0114bc6ad2a9c568077672f0b0555134e47
Richard Garcia <code.richardy@gmail.com>
2026-03-08 13:01:40 -0300

    tests: this commit introduces new global usings to the project, expanding the scope of usings available throughout the project.

--- Boundaries/Comanda.Payments/Tests/Usings.cs
+++ Boundaries/Comanda.Payments/Tests/Usings.cs
@@ -2,7 +2,19 @@ global using Microsoft.AspNetCore.Mvc.Testing;
 global using Microsoft.Extensions.DependencyInjection;
 
 global using Comanda.Payments.WebApi;
+global using Comanda.Payments.Domain.Aggregates;
+global using Comanda.Payments.Domain.Filtering;
+global using Comanda.Payments.Domain.Concepts;
+
+global using Comanda.Payments.Infrastructure.Constants;
+global using Comanda.Payments.Infrastructure.Pipelines;
+
+global using HttpsRichardy.Internal.Essentials.Concepts;
+global using HttpsRichardy.Internal.Essentials.Utilities;
 
 global using MongoDB.Driver;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization;
+
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Containers;
