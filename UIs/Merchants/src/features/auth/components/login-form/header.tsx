e779de17d97e48beeeba33e2a10a1367f4b30477
Richard Garcia <code.richardy@gmail.com>
2026-03-08 22:39:55 -0300

    feat: remove AppLogo from LoginHeader component for a cleaner layout

--- UIs/Merchants/src/features/auth/components/login-form/header.tsx
+++ UIs/Merchants/src/features/auth/components/login-form/header.tsx
@@ -1,12 +1,6 @@
-import { AppLogo } from '@/shared/components/global/logo';
-
 export function LoginHeader() {
   return (
     <div className="flex flex-col items-center gap-2">
-      <div className="flex justify-center items-center">
-        <AppLogo className="w-64" />
-      </div>
-
       <h1 className="text-xl font-bold">OS PEDIDOS TE ESPERAM</h1>
 
       <div className="text-center text-sm">
